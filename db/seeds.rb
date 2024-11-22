bf6e132ae66dc934a9b221e2ff86ce38d85f1672
alvaro bernucci <alvarosenabernucci@hotmail.com>
2024-11-22 16:27:01 -0300

    Projeto Watch List

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "json"
+require "open-uri"
+
+
+api_key = 'd280a7d52f76658fb0585d14c353ba3b'
+url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=5&api_key=#{api_key}"
+
+def fetch_data_from_api(url, headers)
+  open(url, headers) do |response|
+    return response.read
+  end
+end
+
+response = URI.open(url).read
+movies = JSON.parse(response)["results"]
+
+
+movies.each do |movie|
+  Movie.create(
+  title: movie["title"],
+  overview: movie["overview"],
+  poster_url: movie["poster_path"],
+  rating: movie["vote_average"]
+  )
+  p "Create movie: #{movie["title"]}"
+end
+
+
+List.create(name: "Teste inicial")
